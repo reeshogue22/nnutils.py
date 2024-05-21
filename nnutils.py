551d0ac42b67aa41a1ea5a7b4c937c1481677ffc
reeshogue22 <152875103+reeshogue22@users.noreply.github.com>
2024-05-21 05:54:40 -0500

    Add gated attention.

--- nnutils.py
+++ nnutils.py
@@ -10,6 +10,7 @@ import torch
 # 3. ReluSquared
 # 4. GeGLUMLP
 # 5. CausalConv1d
+# 6. GatedAttention
 
 class RMSNorm(torch.nn.Module):
     def __init__(self, indims, epsilon=1e-8):
@@ -49,3 +50,24 @@ class CausalConv1d(torch.nn.Module):
         x = torch.nn.functional.pad(x, (self.conv.kernel_size[0]-1, 0))
         x = self.conv(x)
         return x
+    
+class GatedAttention(torch.nn.Module):
+    def __init__(self, indims, nheads, causal=False):
+        super().__init__()
+        self.qkvr = torch.nn.Linear(indims, indims*4, bias=False)
+        self.proj = torch.nn.Linear(indims, indims, bias=False)
+
+        self.causal = causal
+        self.nheads = nheads
+    def forward(self, x):
+        assert x.dim() == 3
+        q, k, v, r = self.qkvr(x).chunk(4, dim=-1)
+        q = q.view(*q.size()[:-1], self.nheads, q.size(-1)//self.nheads).transpose(1, 2) # (batch, nheads, seq, head)
+        k = k.view(*k.size()[:-1], self.nheads, k.size(-1)//self.nheads).transpose(1, 2) # (batch, nheads, seq, head)
+        v = v.view(*v.size()[:-1], self.nheads, v.size(-1)//self.nheads).transpose(1, 2) # (batch, nheads, seq, head)
+
+        attn = torch.nn.functional.scaled_dot_product_attention(q, k, v, is_causal=self.causal)
+        attn = attn.transpose(1, 2).view(*attn.size()[:-2], attn.size(-2)*attn.size(-1))
+        out = attn * torch.sigmoid(r)
+        out = self.proj(out)
+        return out
